96ed4e0a1893d771f55279afb687bbf7ef26daf6
TJx5x3 <77127097+TJx5x3@users.noreply.github.com>
2025-01-12 15:41:38 -0700

    Test Commit.

--- FlightSimCapstone/Form1.Designer.cs
+++ FlightSimCapstone/Form1.Designer.cs
@@ -33,7 +33,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(119, 286);
+            this.button1.Location = new System.Drawing.Point(67, 87);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(89, 32);
             this.button1.TabIndex = 0;
